f620e303b24b51f8788d7dc839da1538791d72eb
RobStarApps <meyborg@robstar.info>
2014-05-12 18:02:59 +0200

    update_20140512

--- app/controllers/user_skills_controller.rb
+++ app/controllers/user_skills_controller.rb
@@ -1,7 +1,20 @@
 class UserSkillsController < ApplicationController
-  def index
 
+
+
+
+  def index
+ reload_skills
+  @user_skill=UserSkill.new
   end
+
+
+ def show
+  reload_skills
+  @user_skill=UserSkill.new
+   render 'index'
+  end
+
 
   def new
   end
@@ -10,10 +23,13 @@ class UserSkillsController < ApplicationController
   end
 
 	def create
-    @user_skill=Skill.find(user_address_params)
+
+    @user_skill=UserSkill.new(user_skill_params)
     @user_skill.user=current_user
     if  @user_skill.save
+
          @user_skills=current_user.user_skills;
+         reload_skills
       render 'index'
     else
       render 'new'
@@ -26,5 +42,27 @@ class UserSkillsController < ApplicationController
   end
 
   def destroy
+
+     @user_skill=UserSkill.find(params[:id])
+      @user_skill.destroy
+      redirect_to user_skill_path 
+
   end
+
+
+def reload_skills
+    user_skill_ids=Array.new
+    user_skill_ids=current_user.user_skills.group(:skill_id).map(&:skill_id);
+
+    @instruments=Skill.where(:type_id => Type.where(:slug=>'instrument').first.id) & Skill.where.not(:id=>user_skill_ids)
+    @availabilities=Skill.where(:type_id => Type.where(:slug=>'availability').first.id) & Skill.where.not(:id=>user_skill_ids)
+  @transportations=Skill.where(:type_id => Type.where(:slug=>'transportation').first.id) & Skill.where.not(:id=>user_skill_ids)
+
+
+ end
+
+  def user_skill_params 
+  params.require(:user_skill).permit(:skill_id)
+  end
+
 end
