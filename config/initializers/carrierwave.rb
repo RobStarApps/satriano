a5ad1a17fb48cc36dbbcf5d7d9394163004fd505
RobStarApps <meyborg@robstar.info>
2014-05-26 09:19:21 +0200

    daily20140526

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,8 +5,8 @@ CarrierWave.configure do |config|
   config.aws_authenticated_url_expiration = 365.days
 
   config.aws_credentials = {
-    access_key_id:     "AKIAJAMRJB5AJQ7R5IGQ",
-    secret_access_key: "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",
+    access_key_id:     "",
+    secret_access_key: "",
     region: 'eu-west-1'
   }
 end
